214f38a04838144db22d29af3fc1954dd4c61571
Luz Colunga <luz.colunga@udem.edu>
2023-06-05 12:09:55 -0600

    create

--- C/C++/main.cpp
+++ C/C++/main.cpp
@@ -6,5 +6,5 @@ PSP_MODULE_INFO("Tutorial", 0, 1 , 0);
 auto main() ->int{
     pspDebugScreenInit();
 
-    pspDebugScreenPrintf("Hello World from C++!");
+    pspDebugScreenPrintf("This is a test for text display in the PSP");
 }
