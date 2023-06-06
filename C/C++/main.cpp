253d0cb600e535d5fee8a098d97b8f7ba76bcac0
Luz Colunga <luz.colunga@udem.edu>
2023-06-05 21:54:12 -0600

    create

--- C/C++/main.cpp
+++ C/C++/main.cpp
@@ -3,7 +3,28 @@
 
 PSP_MODULE_INFO("Tutorial", 0, 1 , 0);
 
-auto main() ->int{
+int exit_callback(int arg1, int arg2, void* common){
+    sceKernelExitGame();
+    return 0;
+}
+
+int callbackThread(SceSize args, void* argp){
+    int cbid= sceKernelCreateCallback("Exit Callback", exit_callback, NULL);
+    sceKernelRegisterExitCallback(cbid);
+
+    sceKernelSleepThreadCB();
+    return 0;
+}
+
+void setupCallbacks(){
+    int thid = sceKernelCreateThread("update_thread", callbackThread, 0x11, 0xFA0, 0, NULL);
+    if(thid <= 0){
+        SceKernelThread(thid, 0, NULL);
+    }
+}
+
+auto main() ->int {
+    setupCallbacks();
     pspDebugScreenInit();
 
     pspDebugScreenPrintf("This is a test for text display in the PSP");
